bd62f266e86356ba6a43b4ca57ada78adc1c5f47
Patrick Minero <patrickminerovalencia@gmail.com>
2021-01-19 22:51:33 +0100

    Add review model controller and route

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -2,5 +2,6 @@ class Provider < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :bookings
+  has_many :reviews
   validates :name, :service_type, :rate, :location, :description, :languages_spoken, presence: true
 end
