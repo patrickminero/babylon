39337dbe52fcf7fa1eb452e7dba1afcf971ebb49
Patrick Minero <patrickminerovalencia@gmail.com>
2021-01-21 15:37:27 +0100

    Add review to provider#show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+
+    def create
+        @review = Review.new(review_params)
+        @review.user = current_user
+        @provider = Provider.find(params[:provider_id])
+        @review.provider_id = @provider.id
+        if @review.save!
+            redirect_to provider_path(@provider)
+        else
+
+        end
+
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :content)
+    end
 end
